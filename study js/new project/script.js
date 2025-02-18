6e2f76ce795f013b88f6cb807a62ce3fae437c34
lee junghwan <jung20021023@gmail.com>
2025-02-18 11:01:49 +0900

    jwt

--- study js/new project/script.js
+++
@@ -1,8 +0,0 @@
-function search() {
-    let query = document.querySelector(".search-box input").value;
-    if (query) {
-        alert("검색어: " + query);
-    } else {
-        alert("검색어를 입력하세요!");
-    }
-}
