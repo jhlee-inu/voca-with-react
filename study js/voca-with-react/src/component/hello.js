82046108baed21e19bfa81f6141bb54eaef3f6ba
lee junghwan <jung20021023@gmail.com>
2025-03-08 17:58:47 +0900

    #6이벤트처리

---
+++ study js/voca-with-react/src/component/hello.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import World from './world';
+const Hello = () => {
+    function showName() {
+        console.log('Mike');
+    }
+    function showAge(age) {
+        console.log(age);
+    }
+    function showText(e) {
+        console.log(e.target.value);
+    }
+    return (
+        <div>
+            <h1>Hello</h1> 
+            <button onClick={showName}>show me</button>
+            <button
+            onClick={()=>{
+                showAge(10);
+                }}
+                >
+                    show age
+                </button>
+                <input type='text' onChange={(e)=>
+                    console.log(e.target.value)
+                }/>
+            </div>
+    );
+};
+
+export default Hello;
